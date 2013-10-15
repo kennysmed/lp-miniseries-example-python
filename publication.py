661b816c2f0b9cfcf90c1f6855181076ad45ee9e
Phil Gyford <phil@gyford.com>
2013-10-15 11:52:22 +0100

    All working, ETags and everything

--- publication.py
+++ publication.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, send_from_directory
+from datetime import datetime
+from flask import Flask, make_response, render_template, Response, request, send_from_directory
+import hashlib
 
 # Default configuration
 DEBUG = False
 
+# Data for each edition: image filename, description.
 EDITIONS = [
     ['satyre.png', 'Satyre'],
     ['porcupine.png', 'Porcuspine or Porcupine'],
@@ -40,6 +43,8 @@ EDITIONS = [
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
+# If there's a MINISERIES_SETTINGS environment variable, which should be a
+# config filename, use those settings:
 app.config.from_envvar('MINISERIES_SETTINGS', silent=True)
 
 
@@ -54,26 +59,56 @@ def image(image_name):
     return send_from_directory(app.static_folder+'/img', image_name)
 
 
+# Called to generate the sample shown on BERG Cloud Remote.
 @app.route('/sample/')
 def sample():
+    # We can choose which edition we want as the sample:
     delivery_count = 0
-    return render_template('edition.html',
+    response = make_response(render_template(
+                        'edition.html',
                         delivery_count=delivery_count,
-                        image=app.config['EDITIONS'][delivery_count][0],
+                        image_name=app.config['EDITIONS'][delivery_count][0],
                         description=app.config['EDITIONS'][delivery_count][1]
-                    )
+                    ))
+    response.headers['ETag'] = '"%s"' % (
+        hashlib.md5('sample'+datetime.today().strftime('%d%m%Y')).hexdigest() )
+    return response
 
 
+# Called by BERG Cloud to generate publication output to print.
 @app.route('/edition/')
 def edition():
-
     delivery_count = int(request.args.get('delivery_count', 0))
 
-    return render_template('edition.html',
+    if 'local_delivery_time' in request.args:
+        # local_delivery_time is like '2013-10-16T23:20:30-08:00'.
+        # We strip off the timezone, as we only need to know what day this
+        # date is on.
+        date = datetime.strptime(request.args['local_delivery_time'][:-6],
+                                                        '%Y-%m-%dT%H:%M:%S')
+    else:
+        # Default to now.
+        date = datetime.today()
+
+    if (delivery_count + 1) > len(app.config['EDITIONS']):
+        # The publication has finished, so unsubscribe this subscriber.
+        return Response(response=None, status=410)
+
+    elif date.isoweekday() != 3:
+        # No content is delivered this day (Monday=1, Tuesday=2, etc).
+        return Response(response=None, status=204)
+
+    else:
+        # It's all good, so display the publication.
+        response = make_response(render_template(
+                        'edition.html',
                         delivery_count=delivery_count,
-                        image=app.config['EDITIONS'][delivery_count][0],
+                        image_name=app.config['EDITIONS'][delivery_count][0],
                         description=app.config['EDITIONS'][delivery_count][1]
-                    )
+                    ))
+        response.headers['ETag'] = '"%s"' % (
+                                hashlib.md5(str(delivery_count)).hexdigest() )
+        return response
 
 
 if __name__ == '__main__':
